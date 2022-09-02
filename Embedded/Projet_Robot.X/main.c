b9e84073c272a8e585d460b0dbc6b369512fa6f0
TABLE3 <touloniut@gmail.com>
2022-09-02 11:03:45 +0200

    TP1

---
+++ Embedded/Projet_Robot.X/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+#include "ChipConfig.h"
+#include "IO.h"
+#include <xc.h>
+
+int main (void){
+    
+InitOscillator();
+
+
+InitIO();
+
+LED_BLANCHE = 1;
+LED_BLEUE = 1;
+LED_ORANGE = 1;
+
+
+
+while(1){
+    
+} 
+}
